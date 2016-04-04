bf070691bf5be06df6fe52d4a151d9227bcaeaed
sebastianmoore <sebastian.moore@bobcats.gcsu.edu>
2016-04-04 15:04:44 -0400

    Added files via upload

--- header.php
+++ header.php
@@ -1,8 +1,19 @@
 <?php
-
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+function pageHeader($pt)
+{
+    //could put the html for the heading here and use $pt in the title
+    //would keep us from having to have the html on every page and from having to call more than one method and would allow us to have a personallized title for every page if we want
+
+}
+
+function generateTable($arr, $colName)
+{
+    //could have two generate table functions, one for scrolling and another for a grid
+}
+?>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,7 +12,33 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+
+        if($SESSION["loggedin"]){
+
+            //can be done with an sql command setting a boolean to true
+            //could have field in table to represent if it was added in the last month
+            if(NEW PRODUCTED ADDED TO DATABASE WITHIN LAST MONTH){
+                echo "Our newest products.<br><br>";
+
+                //list of products that meet criteria
+                //scrolling menu or something to see them all
+                //similar to amazon's implementation
+            }
+
+            //reset the boolean then set it to true for this statement as well (don't need two booleans here)
+            //could have another field to represent if it was replenished within the last month
+            if(PRODUCT WAS RESTOCKED FROM EMPTY WITHIN THE LAST MONTH && CURRENTLY IN STOCK){
+                echo "Replenished stock.<br><br>";
+
+                //same as above
+            }
+        }else{
+            echo "Log in to access page.";
+
+            //header function to call log in or sign up boxes
+
+        }
+
         ?>
     </body>
 </html>
